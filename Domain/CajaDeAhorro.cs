308c32b2653c9a8c3cb6c4bb20bc1f12ad6e8835
Martina Lopez <martinalopezagueroo@gmail.com>
2025-04-30 22:44:46 -0300

    Puntos 4,5,6

---
+++ Domain/CajaDeAhorro.cs
@@ -0,0 +1,28 @@
+﻿namespace Dsw2025Ej8.Domain
+{
+    public class CajaDeAhorro : CuentaBancaria
+    {
+        public CajaDeAhorro(string numero, decimal saldo, string[] titulares, decimal tasaDeInteres) : base(numero, saldo, titulares)
+        {
+            TasaDeInteres = tasaDeInteres;
+        }
+        public override void Depositar(decimal monto) { 
+            Saldo += monto;
+        }
+
+        public override void Retirar(decimal monto)
+        {
+            Saldo -= monto;
+            if ( Saldo<0)
+                Estado=Estado.Suspendida;
+            
+                
+            }
+        public override void AplicarInteres()
+        {
+            Saldo += Saldo * TasaDeInteres;
+        }
+        
+    }
+}
+
